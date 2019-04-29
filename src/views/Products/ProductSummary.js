3ea6e3fc45b7669b4dbf3a6e06973e55853ea5a6
tharindukumarasiri <ku.tharindu@gmail.com>
2019-04-29 21:26:29 +0800

    updated product view

--- src/views/Products/ProductSummary.js
+++ src/views/Products/ProductSummary.js
@@ -28,6 +28,11 @@ const ProductSummary = ({ product }) => {
         <td key={product.code}>{product.code}</td>
         <td key={product.name}>{product.name}</td>
         <td key={product.authorFirstName}>{product.authorFirstName}</td>
+        <td key={product.category}>{product.category}</td>
+        <td key={product.description}>{product.description}</td>
+        <td key={product.material}>{product.material}</td>
+        <td key={product.price}>{product.price}</td>
+        <td key={product.stock}>{product.stock}</td>
       </tr>
     </div>
 
