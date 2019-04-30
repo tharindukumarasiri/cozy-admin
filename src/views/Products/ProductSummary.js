5fff61124e31794d8babee9d56e5f80e8b320677
tharindukumarasiri <ku.tharindu@gmail.com>
2019-04-30 11:55:22 +0800

    remove comments in summery

--- src/views/Products/ProductSummary.js
+++ src/views/Products/ProductSummary.js
@@ -3,26 +3,6 @@ import { Table } from "reactstrap";
 
 const ProductSummary = ({ product }) => {
   return (
-    // <div>
-    //   <Table responsive>
-    //     <tbody>
-    //       <tr>
-    //         <td>{product.code}</td>
-    //         <td>{product.name}</td>
-    //       </tr>
-    //     </tbody>
-    //   </Table>
-    // </div>
-
-
-    // <div>
-    //   <tr key={product.id}>
-    //     <td>{product.code}</td>
-    //     <td>{product.name}</td>
-    //     <td>{product.authorFirstName}</td>
-    //   </tr>
-    // </div>
-
     <div>
       <tr>
         <td key={product.code}>{product.code}</td>
@@ -35,7 +15,6 @@ const ProductSummary = ({ product }) => {
         <td key={product.stock}>{product.stock}</td>
       </tr>
     </div>
-
   )
 }
 
