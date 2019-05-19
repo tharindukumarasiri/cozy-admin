f4ce5665b89a558e96a7f4af7a220a41a4d8e52f
tharindukumarasiri <ku.tharindu@gmail.com>
2019-05-19 10:18:48 +0530

    added popup model and data submit to database

--- src/views/Branches/Branches.js
+++ src/views/Branches/Branches.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import * as firebase from 'firebase';
 import {
   Button,
+  Input,
+  Label,
   Badge,
   Card,
   CardBody,
@@ -10,17 +13,107 @@ import {
   PaginationItem,
   PaginationLink,
   Row,
-  Table
+  Table,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader
 } from "reactstrap";
 
 class Branches extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '',
+      name: '',
+      number: '',
+      address: '',
+      email: '',
+      maps: '',
+
+      //Model
+      add: false,
+    }
+    this.createBranch = this.createBranch.bind(this);
+    this.toggleAdd = this.toggleAdd.bind(this);
+  }
+
+  onChangeHandler(evt, key) {
+    // const { name, value } = evt.target;
+    // let formErrors = { ...this.state.formErrors };
+
+    this.setState({
+      [key]: evt.target.value
+    });
+
+    // switch (name) {
+    //   case "code":
+    //     formErrors.code =
+    //       value.length < 4 ? "minimum 4 characaters required" : "";
+    //     break;
+    //   case "price":
+    //     formErrors.price = priceRegex.test(value)
+    //       ? ""
+    //       : "invalid price value";
+    //     break;
+    //   default:
+    //     break;
+    // }
+    // this.setState({ formErrors, [name]: value });
+  }
+
+  createBranch() {
+    if (this.state.code !== '' && this.state.name !== '') {
+      firebase.database().ref('branches').push({
+        code: this.state.code,
+        name: this.state.name,
+        number: this.state.number,
+        address: this.state.address,
+        email: this.state.email,
+        maps: this.state.maps
+      })
+      this.toggleAdd();
+    }
+  }
+
+  //Model
+  toggleAdd() {
+    this.setState({
+      add: !this.state.add,
+    });
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
         <Col xs="6" sm="4" md="3" xl="2">
-          <Button block color="success">
-            Add New
-          </Button>
+          <Button block onClick={this.toggleAdd} color="success"> Add New </Button>
+          <Modal
+            {...this.props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            isOpen={this.state.add} toggle={this.toggleAdd} >
+            <ModalHeader toggle={this.toggleAdd}>Add New Branch</ModalHeader>
+            <ModalBody>
+              <Label htmlFor="text-input">Branch Code</Label>
+              <Input type="text" id="code" onChange={(evt) => this.onChangeHandler(evt, 'code')} />
+              <Label htmlFor="text-input">Name</Label>
+              <Input type="text" id="name" onChange={(evt) => this.onChangeHandler(evt, 'name')} />
+              <Label htmlFor="text-input">Contact Number</Label>
+              <Input type="text" id="number" onChange={(evt) => this.onChangeHandler(evt, 'number')} />
+              <Label htmlFor="text-input">Address</Label>
+              <Input type="text" id="address" onChange={(evt) => this.onChangeHandler(evt, 'address')} />
+              <Label htmlFor="text-input">Email</Label>
+              <Input type="email" id="email" onChange={(evt) => this.onChangeHandler(evt, 'email')} />
+              <Label htmlFor="text-input">Google maps link</Label>
+              <Input type="text" id="maps" onChange={(evt) => this.onChangeHandler(evt, 'maps')} />
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" type="reset" onClick={this.createBranch}>Add</Button>{' '}
+              <Button color="secondary" onClick={this.toggleAdd}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
         </Col>
         <br />
 
