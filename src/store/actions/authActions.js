28a8ef169889db56a98f48cdbf88bdf393089ac9
tharindukumarasiri <ku.tharindu@gmail.com>
2019-05-19 20:12:47 +0530

    add logout on authActions.ks

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -35,3 +35,13 @@ export const signUp = (newUser) => {
     });
   }
 }
+
+export const signOut = () => {
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signOut().then(() => {
+      dispatch({ type: 'SIGNOUT_SUCCESS' })
+    });
+  }
+}
