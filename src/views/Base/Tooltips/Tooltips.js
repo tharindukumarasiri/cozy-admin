6a367d9c7e91d029e79068ed1e7f6efec31b5971
tharindukumarasiri <ku.tharindu@gmail.com>
2019-07-01 21:42:32 +0530

    removed tooltips render data

--- src/views/Base/Tooltips/Tooltips.js
+++ src/views/Base/Tooltips/Tooltips.js
@@ -72,60 +72,7 @@ class Tooltips extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        <Card>
-          <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltips</strong>
-            <div className="card-header-actions">
-              <a href="https://reactstrap.github.io/components/tooltips/" rel="noreferrer noopener" target="_blank" className="card-header-action">
-                <small className="text-muted">docs</small>
-              </a>
-            </div>
-          </CardHeader>
-          <CardBody>
-            {/*eslint-disable-next-line*/}
-            <p>Somewhere in here is a <a href="#" id="TooltipExample">tooltip</a>.</p>
-            <Tooltip placement="right" isOpen={this.state.tooltipOpen[0]} target="TooltipExample" toggle={() => {this.toggle(0);}}>
-              Hello world!
-            </Tooltip>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltip</strong>
-            <small> disable autohide</small>
-          </CardHeader>
-          <CardBody>
-            {/*eslint-disable-next-line*/}
-            <p>Sometimes you need to allow users to select text within a <a href="#" id="DisabledAutoHideExample">tooltip</a>.</p>
-            <Tooltip placement="top" isOpen={this.state.tooltipOpen[1]} autohide={false} target="DisabledAutoHideExample" toggle={() => {this.toggle(1);}}>
-              Try to select this text!
-            </Tooltip>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltip</strong>
-            <small> list</small>
-          </CardHeader>
-          <CardBody>
-            {this.state.tooltips.map((tooltip, i) => {
-              return <TooltipItem key={i} item={tooltip} id={i} />;
-            })}
-          </CardBody>
-        </Card>
-        <Card>
-          <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltip</strong>
-            <small> uncontrolled</small>
-          </CardHeader>
-          <CardBody>
-            {/*eslint-disable-next-line*/}
-            <p>Somewhere in here is a <a href="#" id="UncontrolledTooltipExample">tooltip</a>.</p>
-            <UncontrolledTooltip placement="right" target="UncontrolledTooltipExample">
-              Hello world!
-            </UncontrolledTooltip>
-          </CardBody>
-        </Card>
+        
       </div>
     );
   }
