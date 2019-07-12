c09f217bc2bcbb0cac8dc07c88a5fbed65bc3674
tharindukumarasiri <ku.tharindu@gmail.com>
2019-07-12 20:53:43 +0530

    deleted dropdown functionalities

--- src/views/Base/Dropdowns/Dropdowns.js
+++ src/views/Base/Dropdowns/Dropdowns.js
@@ -2,23 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardBody, CardHeader, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Row, UncontrolledDropdown } from 'reactstrap';
 
 class Dropdowns extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: new Array(6).fill(false),
-    };
-  }
-
-  toggle(i) {
-    const newArray = this.state.dropdownOpen.map((element, index) => {
-      return (index === i ? !element : false);
-    });
-    this.setState({
-      dropdownOpen: newArray,
-    });
-  }
+  
 
   render() {
     return (
