4479502ce30c45d72317470c8a3fa61b2d6b816c
tharindukumarasiri <ku.tharindu@gmail.com>
2019-07-13 11:17:05 +0530

    deleted details in carousels

--- src/views/Base/Carousels/Carousels.js
+++ src/views/Base/Carousels/Carousels.js
@@ -9,44 +9,11 @@ class Carousels extends Component {
  
 
     return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col xs="12" xl="6">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i><strong>Carousel</strong>
-                <div className="card-header-actions">
-                  <a href="https://reactstrap.github.io/components/carousel/" rel="noreferrer noopener" target="_blank" className="card-header-action">
-                    <small className="text-muted">docs</small>
-                  </a>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <Carousel activeIndex={activeIndex} next={this.next} previous={this.previous} ride="carousel">
-                  {slides}
-                </Carousel>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col xs="12" xl="6">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i><strong>Carousel</strong>
-              </CardHeader>
-              <CardBody>
-                <Carousel activeIndex={activeIndex} next={this.next} previous={this.previous}>
-                  <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-                  {slides2}
-                  <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-                  <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-                </Carousel>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+      <div >
+        
       </div>
     );
   }
-}
+
 
 export default Carousels;
