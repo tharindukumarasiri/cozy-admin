4b33862b359613b51a613d06eea2f1081c9032d2
tharindukumarasiri <ku.tharindu@gmail.com>
2019-07-17 21:20:43 +0530

    deleted functions in button dropdowns page

--- src/views/Buttons/ButtonDropdowns/ButtonDropdowns.js
+++ src/views/Buttons/ButtonDropdowns/ButtonDropdowns.js
@@ -3,22 +3,7 @@ import { Button, ButtonDropdown, Card, CardBody, CardHeader, Col, DropdownItem, 
 
 class ButtonDropdowns extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: new Array(19).fill(false),
-    };
-  }
-
-  toggle(i) {
-    const newArray = this.state.dropdownOpen.map((element, index) => { return (index === i ? !element : false); });
-    this.setState({
-      dropdownOpen: newArray,
-    });
-  }
-
+ 
   render() {
     return (
       <div className="animated fadeIn">
