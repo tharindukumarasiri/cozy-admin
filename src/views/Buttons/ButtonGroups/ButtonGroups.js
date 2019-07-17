a736a0f0ed7bbabc86b456141a7869e6c9590712
tharindukumarasiri <ku.tharindu@gmail.com>
2019-07-17 21:23:27 +0530

    deleted button groups page functions

--- src/views/Buttons/ButtonGroups/ButtonGroups.js
+++ src/views/Buttons/ButtonGroups/ButtonGroups.js
@@ -20,21 +20,7 @@ import {
 
 class ButtonGroups extends Component {
 
-  constructor(props) {
-    super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: new Array(2).fill(false),
-    };
-  }
-
-  toggle(i) {
-    const newArray = this.state.dropdownOpen.map((element, index) => { return (index === i ? !element : false); });
-    this.setState({
-      dropdownOpen: newArray,
-    });
-  }
 
   render() {
     return (
